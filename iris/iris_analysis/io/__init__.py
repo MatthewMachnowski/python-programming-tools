9ef4dff876994589553bd12bb4cfdb10562e7bf8
Mateusz Machnowski <ma.machnowsk@student.uw.edu.pl>
2021-12-04 20:04:36 +0100

    little changes

---
+++ iris/iris_analysis/io/__init__.py
@@ -0,0 +1,4 @@
+from .load import load_file
+from .save import save_file
+
+__all__ = ['load_file', 'save_file']
--- iris/run_analysis.py
+++ iris/run_analysis.py
@@ -1,13 +1,14 @@
 from iris_analysis import calculate
-from iris_analysis.io.load import load_file
-from iris_analysis.io.save import save_file
+from iris_analysis.io import load_file
+from iris_analysis.io import save_file
 import sys
 
 
 if __name__ == '__main__':
     data = load_file(sys.argv[1])
+
     result = []
     for f in (calculate.mean, calculate.median, calculate.std):
         result.append({key: f(data[key]) for key in data})
-
+        
     save_file(sys.argv[2], result)
